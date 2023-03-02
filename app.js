4e467e928bc1e85d556ea29865b1e791c5ac5af9
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-03-02 00:22:28 -0500

    create a react element

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const title = React.createElement(
   'My First React Element!'
 );
 
-// <h1 id="main-title" title="This is a title"> My First React Element!</h1>
+const desc = React.createElement(
+  'p',
+  null,
+  'I just learned how to create a React node and render it into the DOM.'
+);
+
+const header = React.createElement('header', null, title, desc);
+
+ReactDOM.render(header, document.querySelector('#root'));
 
 console.log(title);
