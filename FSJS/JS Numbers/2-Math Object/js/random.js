386495cc7a7e8e8f1500c56f746d679df123b03c
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-06-26 12:50:16 -0400

    random number challenge

--- FSJS/JS Numbers/2-Math Object/js/random.js
+++ FSJS/JS Numbers/2-Math Object/js/random.js
@@ -1,2 +1,20 @@
-const dieRoll = Math.floor(Math.random() * 6) + 1
-console.log(`You rolled a ${dieRoll}`)
+// const dieRoll = Math.floor(Math.random() * 6) + 1
+// console.log(`You rolled a ${dieRoll}`)
+
+// Collect input from a user
+const inputLow = prompt('Please provide your lowest number')
+const inputHigh = prompt('Please provide your highest number')
+
+// Convert the input to a number
+const lowNumber = parseInt(inputLow)
+const highNumber = parseInt(inputHigh)
+
+if (highNumber && lowNumber) {
+  // Use Math.random() and the user's number to generate a random number
+  const randomNumber = Math.floor(Math.random() * (highNumber - lowNumber) + 1) + lowNumber
+
+  // Create a message displaying the random number
+  document.querySelector('main').innerHTML = `<h1>${randomNumber} is a random number between ${lowNumber} and ${highNumber}!</h1>`
+} else {
+  document.querySelector('main').innerHTML = `<h1>You need to provide two numbers. Please try again!</h1>`
+}
