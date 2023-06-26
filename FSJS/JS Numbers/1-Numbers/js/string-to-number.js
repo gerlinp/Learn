fe7e608ff16d22b0527c91f0764a42c37e419719
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-06-26 11:48:09 -0400

    converting strings to numbers

---
+++ FSJS/JS Numbers/1-Numbers/js/string-to-number.js
@@ -0,0 +1,7 @@
+// const HTMLBadges = prompt('How many HTML badges do you have?')
+// const CSSBadges = prompt('How mand css badges do you have?')
+
+// const totalBadges = +HTMLBadges + +CSSBadges
+
+const pi = '3.14'
+console.log(+pi === 3.14)
