e6c9f74b95fdce43edabcf4ffc9a2d353f695526
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-06-26 02:15:22 -0400

    combine strings

---
+++ FSJS/JS Basics/03 Strings/js/combine.js
@@ -0,0 +1,7 @@
+const name = prompt('What is your name?')
+let message = 'Hello ' + name + '. welcome to my music site. '
+message += "I'm so happy that you came to visit, " + name + '. Feel free to come again and listen to more music!'
+message += name
+message += "Feel free to come again and listen to more music!'"
+
+console.log(message)
