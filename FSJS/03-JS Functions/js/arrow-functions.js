88e6b39cea16989a6a968141feb11e352b0d0b7c
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-06-26 14:25:23 -0400

    more syntax

--- FSJS/03-JS Functions/js/arrow-functions.js
+++ FSJS/03-JS Functions/js/arrow-functions.js
@@ -1,9 +1,17 @@
+//  Arrow function basic syntax
 const getRandomNumber = (upper) => {
   const randomNumber = Math.floor(Math.random() * upper) + 1
   return randomNumber
 }
 
+//  Arrow function  multiple parameter
 const getArea = (width, length, unit) => {
   const area = width * length
   return `${area} ${unit}`
 }
+
+//  Arrow function one parameter
+const square = (x) => {
+  return x * x
+}
+square(10) // 100
