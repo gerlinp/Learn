b40b8c91b98af5c0bee8a2df631acaae1c6b1982
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-06-26 03:17:50 -0400

    boolean values

--- FSJS/JS Basics/04 Conditionals/js/conditionals.js
+++ FSJS/JS Basics/04 Conditionals/js/conditionals.js
@@ -1,6 +1,6 @@
 const answer = prompt('Which planet is closest to the sun?')
 
-if (answer === 'Mercury') {
+if (answer.toUpperCase() === 'MERCURY') {
   console.log("That's Correct!")
 } else {
   console.log("Sorry, that's incorrect!")
