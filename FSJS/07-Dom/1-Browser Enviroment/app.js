85fc3eb855c60f1de80de818a77d99e803cf6dcd
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-06-27 15:16:02 -0400

    modifying elements

--- FSJS/07-Dom/1-Browser Enviroment/app.js
+++ FSJS/07-Dom/1-Browser Enviroment/app.js
@@ -3,6 +3,8 @@ const btnUpdate = document.querySelector('.btn-main')
 btnUpdate.addEventListener('click', () => {
   const input = document.querySelector('.input-main')
   const headline = document.getElementById('headline')
+
+  headline.className = 'grow'
   headline.textContent = input.value
   input.value = ''
 })
