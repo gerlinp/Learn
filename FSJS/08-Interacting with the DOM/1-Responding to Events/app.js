67d402666d32950e0a1def52fc19b51ce5405ffb
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-06-27 17:00:18 -0400

    getting all children of a node with children

--- FSJS/08-Interacting with the DOM/1-Responding to Events/app.js
+++ FSJS/08-Interacting with the DOM/1-Responding to Events/app.js
@@ -2,13 +2,24 @@ const btnCreate = document.getElementById('btn-main')
 const btnToggle = document.querySelector('.btn-toggle')
 const btnRemove = document.querySelector('.btn-remove')
 const taskList = document.querySelector('.list-container ul')
-const listItems = document.getElementsByTagName('li')
+const listItems = taskList.children
 
-taskList.addEventListener('mouseover', (e) => {
-  if (e.target.tagName === 'LI') {
-    e.target.textContent = e.target.textContent.toUpperCase()
-  }
-})
+function attachRemoveButton(li) {
+  let remove = document.createElement('button')
+  remove.className = 'remove'
+  remove.textContent = 'Remove'
+  li.appendChild(remove)
+}
+
+for (let i = 0; i < listItems.length; i++) {
+  attachRemoveButton(listItems[i])
+}
+
+// taskList.addEventListener('mouseover', (e) => {
+//   if (e.target.tagName === 'LI') {
+//     e.target.textContent = e.target.textContent.toUpperCase()
+//   }
+// })
 
 btnToggle.addEventListener('click', () => {
   const listContainer = document.querySelector('.list-container')
@@ -22,9 +33,12 @@ btnToggle.addEventListener('click', () => {
 })
 
 btnCreate.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0]
   const input = document.querySelector('.input-main')
-  const list = document.querySelector('ul')
-  list.insertAdjacentHTML('afterbegin', `<li>${input.value}</i>`)
+  let li = document.createElement('li')
+  li.textContent = input.value
+  attachRemoveButton(li)
+  ul.appendChild(li)
   input.value = ''
 })
 
