8e832e5e5a65dd2233b1dbb81ee105036b210010
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-06-27 15:55:09 -0400

    insert at specified positions

--- FSJS/07-Dom/1-Browser Enviroment/app.js
+++ FSJS/07-Dom/1-Browser Enviroment/app.js
@@ -5,6 +5,10 @@ btnCreate.addEventListener('click', () => {
   const input = document.querySelector('.input-main')
   const list = document.querySelector('ul')
 
+  list.insertAdjacentHTML(
+    'afterbegin', //
+    `<li>${input.value}</li>`
+  )
   input.value = ''
 })
 
