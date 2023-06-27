f5ad2465f0532e688226edb969a0f48fefeecd5f
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-06-26 22:54:19 -0400

    remove elements from array

---
+++ FSJS/05-Arrays/js/remove.js
@@ -0,0 +1,11 @@
+const numbers = [100, 200, 300, 400, 500]
+
+numbers.push('600')
+console.log(numbers)
+numbers.pop()
+console.log(numbers)
+
+numbers.unshift('700')
+console.log(numbers)
+numbers.shift()
+console.log(numbers)
