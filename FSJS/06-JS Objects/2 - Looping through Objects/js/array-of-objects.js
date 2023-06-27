a6e9aab3651be2fb72baba34e41deb3cddd3c6e0
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-06-27 12:19:00 -0400

    storing objects in arrays

--- FSJS/06-JS Objects/2 - Looping through Objects/js/array-of-objects.js
+++ FSJS/06-JS Objects/2 - Looping through Objects/js/array-of-objects.js
@@ -1,15 +1,27 @@
 // 1. Create an array to hold quiz questions and answers
 const questions = [
-  ['How many planets are in the Solar System?', '8'],
-  ['How many continents are there?', '7'],
-  ['How many legs does an insect have?', '6'],
-  ['What year was JavaScript created?', '1995']
-];
+  {
+    question: 'How many planets are in the Solar System?',
+    answer: '8',
+  },
+  {
+    question: 'How many continents are there?',
+    answer: '7',
+  },
+  {
+    question: 'How many legs does an insect have?',
+    answer: '6',
+  },
+  {
+    question: 'What year was JavaScript created?',
+    answer: '1995',
+  },
+]
 
 // 2. Store the number of questions answered correctly
-const correct = [];
-const incorrect = [];
-let correctAnswers = 0;
+const correct = []
+const incorrect = []
+let correctAnswers = 0
 
 /* 
   3. Use a loop to cycle through each question
@@ -18,35 +30,35 @@ let correctAnswers = 0;
       - If the response matches the answer, the number of correctly
         answered questions increments by 1
 */
-for ( let i = 0; i < questions.length; i++ ) {
-  let question = questions[i][0];
-  let answer = questions[i][1];
-  let response = prompt(question);
-  
-  if ( response === answer ) {
-    correctAnswers++;
-    correct.push(question);
+for (let i = 0; i < questions.length; i++) {
+  let question = questions[i].question
+  let answer = questions[i].answer
+  let response = prompt(question)
+
+  if (response === answer) {
+    correctAnswers++
+    correct.push(question)
   } else {
-    incorrect.push(question);
+    incorrect.push(question)
   }
 }
 
 function createListItems(arr) {
-  let items = '';
+  let items = ''
   for (let i = 0; i < arr.length; i++) {
-    items += `<li>${arr[i]}</li>`;
+    items += `<li>${arr[i]}</li>`
   }
-  return items;
+  return items
 }
 
 // 4. Display the number of correct answers to the user
 let html = `
   <h1>You got ${correctAnswers} question(s) correct</h1>
   <h2>You got these questions right:</h2>
-  <ol>${ createListItems(correct) }</ol>
+  <ol>${createListItems(correct)}</ol>
 
   <h2>You got these questions wrong:</h2>
-  <ol>${ createListItems(incorrect) }</ol>
-`;
+  <ol>${createListItems(incorrect)}</ol>
+`
 
-document.querySelector('main').innerHTML = html;
+document.querySelector('main').innerHTML = html
