71fdffa0ca731d16532afa608a139452ac0d3316
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-06-27 14:26:49 -0400

    select by tag name

--- FSJS/07-Dom/1-Browser Enviroment/app.js
+++ FSJS/07-Dom/1-Browser Enviroment/app.js
@@ -2,11 +2,17 @@ const headline = document.getElementById('headline')
 const btnUpdate = document.getElementById('btn-main')
 const randomRGBColor = () => Math.floor(Math.random() * 256)
 
-function randomRGB(value) {
-  return `rgb( ${value()}, ${value()}, ${value()} )`
+const items = document.getElementsByTagName('li')
+
+for (let i = 0; i < items.length; i++) {
+  items[i].style.backgroundColor = 'orchid'
 }
 
 btnUpdate.addEventListener('click', () => {
   headline.style.border = `solid 10px ${randomRGB(randomRGBColor)}`
   headline.style.fontSize = '60px'
 })
+
+function randomRGB(value) {
+  return `rgb( ${value()}, ${value()}, ${value()} )`
+}
