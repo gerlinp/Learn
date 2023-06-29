8694c2667230a45556e38e609916dd78a29e3dbf
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-06-28 23:50:37 -0400

    Converting callbacks to promises

--- FSJS/10-Asynchronous/js/promises.js
+++ FSJS/10-Asynchronous/js/promises.js
@@ -1,30 +1,36 @@
-const astrosUrl = 'http://api.open-notify.org/astros.json';
-const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
-const peopleList = document.getElementById('people');
-const btn = document.querySelector('button');
+const astrosUrl = 'http://api.open-notify.org/astros.json'
+const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
+const peopleList = document.getElementById('people')
+const btn = document.querySelector('button')
 
-function getJSON(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = () => {
-    if(xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      return callback(data);
+function getJSON(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', url)
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        let data = JSON.parse(xhr.responseText)
+        resolve(data)
+      } else {
+        reject(Error(xhr.statusText))
+      }
     }
-  };
-  xhr.send();
+    xhr.onerror = () => reject(Error('A network error occured'))
+    xhr.send()
+  })
 }
 
 function getProfiles(json) {
-  json.people.map( person => {
-    getJSON(wikiUrl + person.name, generateHTML);      
-  }); 
+  const profiles = json.people.map((person) => {
+    return getJSON(wikiUrl + person.name, generateHTML)
+  })
+  return profiles
 }
 
 // Generate the markup for each profile
 function generateHTML(data) {
-  const section = document.createElement('section');
-  peopleList.appendChild(section);
+  const section = document.createElement('section')
+  peopleList.appendChild(section)
   // Check if request returns a 'standard' page from Wiki
   if (data.type === 'standard') {
     section.innerHTML = `
@@ -32,18 +38,21 @@ function generateHTML(data) {
       <h2>${data.title}</h2>
       <p>${data.description}</p>
       <p>${data.extract}</p>
-    `;
+    `
   } else {
     section.innerHTML = `
       <img src="img/profile.jpg" alt="ocean clouds seen from space">
       <h2>${data.title}</h2>
       <p>Results unavailable for ${data.title}</p>
       ${data.extract_html}
-    `;
+    `
   }
 }
 
 btn.addEventListener('click', (event) => {
-  getJSON(astrosUrl, getProfiles);
-  event.target.remove();
-});
+  getJSON(astrosUrl)
+    .then(getProfiles)
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err))
+  event.target.remove()
+})
