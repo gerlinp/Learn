c35535c0a7876f450c872e17969a98017552aad7
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-07-01 23:47:18 -0400

    Displaying the content

--- FSJS/11-Fetch-API/js/app.js
+++ FSJS/11-Fetch-API/js/app.js
@@ -5,13 +5,32 @@ const form = document.querySelector('form')
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
+fetch('https://dog.ceo/api/breeds/list')
+  .then((response) => response.json())
+  .then((data) => generateOptions(data.message))
+
 fetch('https://dog.ceo/api/breeds/image/random')
   .then((response) => response.json())
-  .then((data) => console.log(data.message))
+  .then((data) => generateImage(data.message))
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+function generateOptions(data) {
+  const options = data.map(
+    (item) => `
+  <option value ='${item}'></option>`
+  )
+  select.innerHTML = options
+}
+
+function generateImage(data) {
+  const html = `
+        <img src='${data}' alt> 
+        <p>CLick to view images of ${select.value}s</p>
+    `
+  card.innerHTML = html
+}
 
 // ------------------------------------------
 //  EVENT LISTENERS
