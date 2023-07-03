472a9054859462a35fa0969dcfbe62bff53b4f62
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-07-02 23:40:42 -0400

    Reusable fetch function

--- FSJS/11-Fetch-API/js/app.js
+++ FSJS/11-Fetch-API/js/app.js
@@ -5,36 +5,55 @@ const form = document.querySelector('form')
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
-fetch('https://dog.ceo/api/breeds/list')
-  .then((response) => response.json())
-  .then((data) => generateOptions(data.message))
 
-fetch('https://dog.ceo/api/breeds/image/random')
-  .then((response) => response.json())
-  .then((data) => generateImage(data.message))
+function fetchData(url) {
+  return fetch(url).then((res) => res.json())
+}
+
+fetchData('https://dog.ceo/api/breeds/list').then((data) => generateOptions(data.message))
+
+fetchData('https://dog.ceo/api/breeds/image/random').then((data) => generateImage(data.message))
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+
 function generateOptions(data) {
-  const options = data.map(
-    (item) => `
-  <option value ='${item}'></option>`
-  )
+  const options = data
+    .map(
+      (item) => `
+    <option value='${item}'>${item}</option>
+  `
+    )
+    .join('')
   select.innerHTML = options
 }
 
 function generateImage(data) {
   const html = `
-        <img src='${data}' alt> 
-        <p>CLick to view images of ${select.value}s</p>
-    `
+    <img src='${data}' alt>
+    <p>Click to view images of ${select.value}s</p>
+  `
   card.innerHTML = html
+}
+
+function fetchBreedImage() {
+  const breed = select.value
+  const img = card.querySelector('img')
+  const p = card.querySelector('p')
+
+  fetchData(`https://dog.ceo/api/breed/${breed}/images/random`).then((data) => {
+    img.src = data.message
+    img.alt = breed
+    p.textContent = `Click to view more ${breed}s`
+  })
 }
 
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
+select.addEventListener('change', fetchBreedImage)
+card.addEventListener('click', fetchBreedImage)
 
 // ------------------------------------------
 //  POST DATA
