945f4051a7b5c8225194b023e325ba4d2bda957a
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-07-07 02:35:09 -0400

    Dot notation and Bracket notation

---
+++ FSJS/12-Object-Oriented-Programming/Object-Basics/pets.js
@@ -0,0 +1,15 @@
+const ernie = {
+  animal: 'dog',
+  age: '1',
+  breed: 'pug',
+  bark: function () {
+    console.log('Woof!')
+  },
+}
+
+console.log(ernie['age'])
+console.log(ernie['breed'])
+ernie['bark']()
+
+var prop = 'breed'
+ernie[prop]
