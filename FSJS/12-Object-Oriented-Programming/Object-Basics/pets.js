265e2db29f0715afbaefa0cd6fb709965e3e55b0
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-07-08 01:38:51 -0400

    First class

--- FSJS/12-Object-Oriented-Programming/Object-Basics/pets.js
+++ FSJS/12-Object-Oriented-Programming/Object-Basics/pets.js
@@ -7,8 +7,29 @@ const ernie = {
   },
 }
 
-ernie.age = 2
-ernie['age'] = 2
+const vera = {
+  animal: 'dog',
+  age: 8,
+  breed: 'Border Collie',
+  bark: function () {
+    console.log('Woof!')
+  },
+}
 
-ernie.color = 'black'
-console.log(ernie)
+const scofield = {
+  animal: 'dog',
+  age: 6,
+  breed: 'Doberman',
+  bark: function () {
+    console.log('Woof!')
+  },
+}
+
+const edel = {
+  animal: 'dog',
+  age: 7,
+  breed: 'German Shorthaired Pointer',
+  bark: function () {
+    console.log('Woof!')
+  },
+}
