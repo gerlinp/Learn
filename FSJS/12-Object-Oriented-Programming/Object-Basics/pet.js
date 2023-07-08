cad36843ba6cae9dc4aaaf7bdf193f56d551e23a
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-07-08 02:00:21 -0400

    adding methods to classes

--- FSJS/12-Object-Oriented-Programming/Object-Basics/pet.js
+++ FSJS/12-Object-Oriented-Programming/Object-Basics/pet.js
@@ -1,12 +1,18 @@
 class Pet {
-  constructor(animal, age, breed) {
+  constructor(animal, age, breed, sound) {
     this.animal = animal
     this.age = age
     this.breed = breed
+    this.sound = sound
+  }
+
+  speak() {
+    console.log(this.sound)
   }
 }
 
-const ernie = new Pet('dog', 1, 'pug')
-const vera = new Pet('dog', 8, 'border collie')
+const ernie = new Pet('dog', 1, 'pug', 'yip yip')
+const vera = new Pet('dog', 8, 'border collie', 'woof woof')
 
-console.log(ernie, vera)
+ernie.speak()
+vera.speak()
