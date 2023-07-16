5c28b08dffa0fc9fdb59479d7fcb7b4b6678210c
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-07-16 12:17:34 -0400

    Update app.js

--- FSJS/12-Object-Oriented-Programming/OOPJS-Challenge/js/app.js
+++ FSJS/12-Object-Oriented-Programming/OOPJS-Challenge/js/app.js
@@ -1,8 +1,11 @@
-const game = new Gamepad()
+const game = new Game()
 
-document.querySelector('#begin-game').addEventListener('click', function () {
+document.getElementById('begin-game').addEventListener('click', function () {
   game.startGame()
-
   this.style.display = 'none'
-  document.querySelector('#play-area').style.opacity = '1'
+  document.getElementById('play-area').style.opacity = '1'
+})
+
+document.addEventListener('keydown', function (event) {
+  game.handleKeydown(event)
 })
