0118b5829fbabdb3bc39a23a01836e7870a73b08
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-07-17 23:51:49 -0400

    Update Board.js

--- FSJS/12-Object-Oriented-Programming/OOPJS-Challenge/js/Board.js
+++ FSJS/12-Object-Oriented-Programming/OOPJS-Challenge/js/Board.js
@@ -2,21 +2,21 @@ class Board {
   constructor() {
     this.rows = 6
     this.columns = 7
-    this.spaces = []
+    this.spaces = this.createSpaces()
   }
 
-  createSpace() {
+  createSpaces() {
     const spaces = []
 
     for (let x = 0; x < this.columns; x++) {
-      const column = []
+      const col = []
 
-      for (let x = 0; x < this.columns; x++) {
+      for (let y = 0; y < this.rows; y++) {
         const space = new Space(x, y)
-        column.push(space)
+        col.push(space)
       }
 
-      spaces.push(column)
+      spaces.push(col)
     }
 
     return spaces
