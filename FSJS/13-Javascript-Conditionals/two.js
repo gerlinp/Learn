9e7163c85918bd66f10ea7071333622cd9c6c638
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-07-19 23:37:07 -0400

    ternary operator

--- FSJS/13-Javascript-Conditionals/two.js
+++ FSJS/13-Javascript-Conditionals/two.js
@@ -1,7 +1,11 @@
-var isTrue = true;
+var isTrue = true
 
-if(isTrue) {
-	console.log('yes');
-} else {
-	console.log('no');
-}
+// if (isTrue) {
+//   console.log('yes')
+// } else {
+//   console.log('no')
+// }
+
+let yesOrNo = isTrue ? 'yes' : 'no'
+
+console.log(yesOrNo)
