eb3ca8dd09dbb3dc4f050aa9793caa4a675363ca
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-07-23 08:10:08 -0400

    server

--- FSJS/14-Node.js/app.js
+++ FSJS/14-Node.js/app.js
@@ -7,25 +7,27 @@ var http = require('http')
 http
   .createServer(function (req, res) {
     res.createServer(200, { 'Content-Type': 'text/plain' })
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.write('Hello World\n')
     res.end('Hello World\n')
+    res.write('This is after the end\n')
   })
   .listen(3000, '127.0.0.1')
 console.log('Server running at http://127.0.0.1:3000/')
 
 // 2. Handle the HTTP route GET/ and POST / i.e Home
-    // if url == '/' && GET
-        // show search
-    // if url  === '/' && POST
-        // redirect to /:username
+// if url == '/' && GET
+// show search
+// if url  === '/' && POST
+// redirect to /:username
 
 // 3. Handle the HTTP route GET /: username i.e. /chalkers
-    // if url == '/....'
-        // get json from Treehouse
-            // on 'end'
-                // show profile
-            // on 'error'
-                // show error
+// if url == '/....'
+// get json from Treehouse
+// on 'end'
+// show profile
+// on 'error'
+// show error
 // 4. function that handles the reading of files and merge in value
-    // read from file and get a string
-        // merge values in to string
-
+// read from file and get a string
+// merge values in to string
