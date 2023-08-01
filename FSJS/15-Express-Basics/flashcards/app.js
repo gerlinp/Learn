9fd296ec5811f0660e08a011c1e73142a4a81b75
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-08-01 13:12:25 -0400

    request object

--- FSJS/15-Express-Basics/flashcards/app.js
+++ FSJS/15-Express-Basics/flashcards/app.js
@@ -1,28 +1,29 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
-app.use(bodyParser.urlencoded({extended: false}))
+const app = express();
 
-app.set('view engine', 'pug')
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('index')
-})
+    res.render('index');
+});
 
 app.get('/cards', (req, res) => {
-  res.render('card', {prompt: 'Who is buried in Grant\'s tomb?'})
-})
+    res.render('card', { prompt: "Who is buried in Grant's tomb?" });
+});
 
 app.get('/hello', (req, res) => {
-  res.render('hello') 
-})
+  res.render('hello');
+});
 
 app.post('/hello', (req, res) => {
-  console.log(req.body)
-  res.render('hello') 
-})
+  console.dir(req.body);
+  res.render('hello');
+});
 
 app.listen(3000, () => {
-  console.log('The application is running on localhost:3000!')
-})
+    console.log('The application is running on localhost:3000!')
+});
