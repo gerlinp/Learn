ee2cbfe83a5edc9fdfbfc0f54e456945b215f908
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-08-05 23:41:32 -0400

    Handling 404 errors

--- FSJS/15-Express-Basics/flashcards/app.js
+++ FSJS/15-Express-Basics/flashcards/app.js
@@ -9,18 +9,6 @@ app.use(cookieParser())
 
 app.set('view engine', 'pug')
 
-app.use((req, res, next) => {
-  console.log('Hello')
-  const err = new Error('Oh noes!')
-  err.status = 500
-  next(err)
-})
-
-app.use((req, res, next) => {
-  console.log('World')
-  next()
-})
-
 app.get('/', (req, res) => {
   const name = req.cookies.username
   if (name) {
@@ -51,6 +39,12 @@ app.post('/hello', (req, res) => {
 app.post('/goodbye', (req, res) => {
   res.clearCookie('username')
   res.redirect('/hello')
+})
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
 })
 
 app.use((err, req, res, next) => {
