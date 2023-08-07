2b93703d6758959aaa04d9ac29bec18a24219b87
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-08-07 13:58:31 -0400

    Complete

--- FSJS/15-Express-Basics/flashcards/routes/cards.js
+++ FSJS/15-Express-Basics/flashcards/routes/cards.js
@@ -6,16 +6,21 @@ const { cards } = data
 router.get('/', (req, res) => {
   const numberOfCards = cards.length
   const flashcardId = Math.floor(Math.random() * numberOfCards)
-  res.redirect(`/cards/${flashcardId}?side=question`)
+  res.redirect(`/cards/${flashcardId}`)
 })
 
 router.get('/:id', (req, res) => {
   const { side } = req.query
   const { id } = req.params
+
+  if (!side) {
+    res.redirect(`/cards/${id}?side=question`)
+  }
+  const name = req.cookies.username
   const text = cards[id][side]
   const { hint } = cards[id]
 
-  const templateData = { id, text }
+  const templateData = { id, text, name }
 
   if (side === 'question') {
     templateData.hint = hint
