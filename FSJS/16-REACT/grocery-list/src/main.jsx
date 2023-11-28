1835b1fe30d9ee546ae8d091fcc3abaa8852ff2a
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-11-28 09:43:33 -0500

    Update main.jsx

--- FSJS/16-REACT/grocery-list/src/main.jsx
+++ FSJS/16-REACT/grocery-list/src/main.jsx
@@ -1,11 +1,15 @@
 import { createRoot } from 'react-dom/client'
 
+const desc =
+  'I just learned how to create a React node and render it into the DOM'
+const myTitleID = 'main-title'
+const name = 'Gerlin'
+
 const element = (
   <>
-    <h1>My First React Element</h1>
-    <p class='main-desc'>
-      I just learned how to create a React node and render it into the DOM
-    </p>
+    <h1 id={myTitleID}>{name}'s First React Element</h1>
+    <p class='main -desc'>{desc}</p>
+    <input value={10 * 20} />
   </>
 )
 
